c8b532378393550c9e076aae362b4c51e531a250
YaoWang <yao.wang@vrviu.com>
2018-01-22 13:18:22 +0800

    setting result write into ini file.

--- setting.pas
+++ setting.pas
@@ -3,8 +3,8 @@ unit setting;
 interface
 
 uses
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, ExtCtrls, Buttons, FileCtrl, main;
+  Windows, Messages, SysUtils, Classes, Forms, StdCtrls, ExtCtrls, Buttons,
+  FileCtrl, Controls, inifiles;
 
 type
   TForm2 = class(TForm)
@@ -14,7 +14,6 @@ type
     CheckBox1: TCheckBox;
     ComboBox1: TComboBox;
     CheckBox2: TCheckBox;
-    OpenDialog1: TOpenDialog;
     BitBtn1: TBitBtn;
     procedure SpeedButton1Click(Sender: TObject);
     procedure BitBtn1Click(Sender: TObject);
@@ -30,18 +29,36 @@ var
 
 implementation
 
+uses main;
+
 {$R *.dfm}
 
 procedure TForm2.BitBtn1Click(Sender: TObject);
+var
+  ini_filename, m : string;
+  ini_file: TInifile;
 begin
   if (LabeledEdit1.Text <> Form1.outfolder) OR
      (RadioGroup1.Items[RadioGroup1.ItemIndex] <> Form1.extension) OR
      (CheckBox1.Checked) OR
      (CheckBox2.Checked) then
   begin
+    if NOT DirectoryExists(LabeledEdit1.Text) then
+      SpeedButton1Click(Self);
     Form1.outfolder := LabeledEdit1.Text;
     Form1.extension := RadioGroup1.Items[RadioGroup1.ItemIndex];
     Form1.use_segment_mode := CheckBox1.Checked;
+    if Form1.use_segment_mode then
+      m := '1'
+    else
+      m := '0';
+
+    ini_filename := ExtractFilePath(paramstr(0)) + 'setting.ini';
+    ini_file := TInifile.Create(ini_filename);
+    ini_file.WriteString('setting', 'outfolder', Form1.outfolder);
+    ini_file.WriteString('setting', 'extension', Form1.extension);
+    ini_file.WriteString('setting', 'use_segment_mode', m);
+    ini_file.Free;
   end;
   Form2.Hide;
 end;
@@ -57,7 +74,7 @@ begin
       RadioGroup1.ItemIndex := i;
       break;
     end;
-
+  CheckBox1.Checked := Form1.use_segment_mode;
 end;
 
 procedure TForm2.SpeedButton1Click(Sender: TObject);
@@ -66,6 +83,7 @@ var
 begin
   Root := '';
   Caption := 'Please Select Output Folder:';
+  Directory := LabeledEdit1.Text;
   if SelectDirectory(Caption, Root, Directory) then
   begin
     if Directory <> '' then
