a6d89040d78afc4019af959a40e4e04f97ddef1c
YaoWang <yao.wang@vrviu.com>
2018-01-22 16:03:22 +0800

    update project files

--- vct.dpr
+++ vct.dpr
@@ -3,12 +3,14 @@ program vct;
 uses
   Forms,
   main in 'main.pas' {Form1},
-  utils in 'utils.pas';
+  utils in 'utils.pas',
+  setting in 'setting.pas' {Form2};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TForm2, Form2);
   Application.Run;
 end.
